5391c9402bf03736aff0cd289f0e4522fad4f2ae
Dennis <55805591+klimenkoff9@users.noreply.github.com>
2020-10-25 22:45:58 -0400

    Lab 8

---
+++ Lab#8/SchemeWiki.scm
@@ -0,0 +1,15 @@
+Lab#8
+Scheme Learning Experience
+
+Scheme is a very hard language to understand and work with, way harder than more sociaty acceptable languages such as Java or Python. 
+Even C didn't look that hard in comparision with Scheme.
+The syntax used for mathematical operations is infix notation which is a little different. 
+The biggest trouble were the parentheses and now I will never be mad on Java for being so difficult with semicolumns. 
+The idea that you need to use parentheses before writing the method, and the method goes inside the parentheses was a completely a new thing for me. 
+Writing lambda expressions was tricky because I had to use recursion, however it helped me to understand it better (even with  other languages).
+Running Scheme was done by changing the mode of the file making it visible and callingit through it's path. 
+The thinking that had to be done through conditionals was also different, and making liststs reminded me of arrays that were created. I had to make sure I used '() and defined the list before hand so I don't have to create the list everytime. 
+It was an interesting exeprience making different algorithms, that I've done in Java, but the way of writing it made a lot of change.
+If Python and Java were English and French, Scheme is Russian.  Overall it was similar in logic but the syntax made a big difference.
+My IDE also wasn't very frienly with Scheme so I had to make up some space on my computer for DrRacket IDE. 
+A good thing that my previous expirience tought me to watch YouTube in order to find help with programming and it helped me out this time too.
